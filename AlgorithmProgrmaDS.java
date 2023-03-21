29dd20de7be8a82063f95b8fce0365649489b455
gaurav459433 <gaurav459433@gmail.com>
2023-03-21 19:38:33 +0530

    Added: Code For Permutations Of a String

--- AlgorithmProgrmaDS.java
+++ AlgorithmProgrmaDS.java
@@ -3,5 +3,22 @@ package assignments259.algorithmprogramsds;
 public class AlgorithmProgrmaDS {
     public static void main(String[] args) {
         System.out.println("Welcome To Data Structure Algorithm Programs: ");
+        String s = "abc";
+        printPermutation(s, " ");
+    }
+
+    // Program-1: Method For Permutations
+    static void printPermutation(String str, String ans) {
+        //If String is Empty
+        if (str.length() == 0) {
+            System.out.println(ans + " ");
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            // Rest of the string after excluding the ith character
+            String s1 = str.substring(0, i) + str.substring(i + 1);
+            // Recursive call
+            printPermutation(s1, ans + ch);
+        }
     }
 }
