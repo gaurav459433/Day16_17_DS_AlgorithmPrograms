8018610b1d7e11d366cf177399970eb96f21e94e
gaurav459433 <gaurav459433@gmail.com>
2023-03-21 19:13:40 +0530

    Welcome To Data Structure Algorithm Programs:

---
+++ AlgorithmProgrmaDS.java
@@ -0,0 +1,7 @@
+package assignments259.algorithmprogramsds;
+
+public class AlgorithmProgrmaDS {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Data Structure Algorithm Programs: ");
+    }
+}
